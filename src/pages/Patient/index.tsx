1d42c8a1443805434daad4ef08bb66bb083a2a56
mathiastsq <mathias.matos@townsq.com.br>
2025-01-08 21:49:02 -0300

    POST patient criada
    Falta povoar ou mockar os outros atributos que o backEnd espera

--- src/pages/Patient/index.tsx
+++ src/pages/Patient/index.tsx
@@ -1,23 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { getPatients } from '../../services/patientService';
+import { createPatient, getPatients } from '../../services/patientService';
 
 
-
+//Interface pra definir o que tem nessa entidade
 interface Patient {
   id: string;
   person: {
     name: string;
-    cpf: string; 
+    cpf: string;
     gender: string;
   };
 }
 const Paciente: React.FC = () => {
-  //patients é a variável de estado, recebe a lista do get
-  //setPatients é atualiza patients
-  //useState inicializa com vetor vazio
-  const [patients, setPatients] = useState<Patient[]>([]);
 
+  //Estado p/ receber do GET [variável de estado, método atualizador da variável]
+  const [patients, setPatients] = useState<Patient[]>([]); //useState inicializa com array vazio
+
+
+  //Estado p/ guardar input do user no form [objeto novoPaciente, método atualizador do objeto]  
+  const [newPatient, setNewPatient] = useState({
+    name: '',
+    cpf: '',
+    gender: '',
+  })
+
+  //chamando API GET
   useEffect(() => {
     const fetchPatients = async () => {
       //chama getPatients do service
@@ -26,8 +34,27 @@ const Paciente: React.FC = () => {
     };
 
     fetchPatients(); //chamando a função 
-  }, []); 
+  }, []); //array vazio é como faz isso ser carregado apenas uma vez (quando chama o componente)
 
+
+  //POST
+  /*Método pra atualizar o estado conforme input
+  evento captura mudanças no input ou no select
+  extrai os valores dessa entrada do user e atualiza o estado copiando os valores existentes*/
+  const handleUserInput = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target
+    /*const name = e.target.name;
+    const value = e.target.value;*/
+    setNewPatient({ ...newPatient, [name]: value });
+  }
+
+  //POST
+  //Função p/ enviar dados 
+  const handleCreatePatient = async () => {
+    await createPatient(newPatient) //envia pro back
+    const updatedPatients = await getPatients(); //atualiza lista (novo get)
+    setPatients(updatedPatients);
+  }
 
   //Render abaixo
   return (
@@ -35,18 +62,50 @@ const Paciente: React.FC = () => {
       <h1>Página do Paciente</h1>
       <Link to="/">Voltar para Home</Link>
 
-      <h2>Pacientes:</h2>
-    <ul>
-      {patients.map((patient) => (
-        <li key={patient.id}>
-          Nome: {patient.person.name} 
-         <li>CPF: {patient.person.cpf} </li> 
-          <li>Gênero: {patient.person.gender}</li>
 
-        </li>
-      ))}
-    </ul>
-  </div>
+      {/* Exibição da lista de Pacientes */}
+      <h2>Pacientes:</h2>
+      <ul>
+        {patients.map((patient) => (
+          <li key={patient.id}>
+            Nome: {patient.person.name}
+            <li>CPF: {patient.person.cpf} </li>
+            <li>Gênero: {patient.person.gender}</li>
+
+          </li>
+        ))}
+
+
+      </ul>
+
+      <div>
+        <h3>Adicionar Paciente</h3>
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome"
+          value={newPatient.name}
+          onChange={handleUserInput}
+        />
+        <input
+          type="text"
+          name="cpf"
+          placeholder="CPF"
+          value={newPatient.cpf} // Valor atual do campo "cpf" no estado
+          onChange={handleUserInput} // Atualiza o estado quando o usuário digita
+        />
+        <select
+          name="gender"
+          value={newPatient.gender} // Valor atual do campo "gender" no estado
+          onChange={handleUserInput} // Atualiza o estado quando o usuário seleciona uma opção
+        >
+          <option value="">Selecione o gênero</option>
+          <option value="MALE">Masculino</option>
+          <option value="FEMALE">Feminino</option>
+        </select>
+        <button onClick={handleCreatePatient}>Adicionar Paciente</button>
+      </div>
+    </div>
   );
 };
 
--- src/services/patientService.ts
+++ src/services/patientService.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/patients'; 
+const API_URL = 'http://localhost:3000/patients';
 
 export const getPatients = async () => {
-  try {
-    const response = await axios.get(API_URL);
-    return response.data;
-  } catch (error) {
-    console.error('Erro ao buscar pacientes:', error);
-    return []; 
-  }
+  const response = await axios.get(API_URL);
+  return response.data;
+
 };
+
+
+export const createPatient = async (patientData: any) => {
+  const response = await axios.post(API_URL, patientData);
+  return response.data;
+}
